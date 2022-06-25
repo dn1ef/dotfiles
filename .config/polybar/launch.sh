e27e8bb6a6a1c962f397253fda971acc93575f65
dn1ef <d.nguyen24@yahoo.com>
2022-06-24 18:56:42 -0600

    add polybar folder

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+dir="$HOME/.config/polybar"
+themes=(`ls --hide="launch.sh" $dir`)
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar main --config=~/.config/polybar/polybar-rounded/config.ini &
+polybar second --config=~/.config/polybar/polybar-rounded/config.ini &
